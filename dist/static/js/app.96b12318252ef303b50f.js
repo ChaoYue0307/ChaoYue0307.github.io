c2504958f7b25c8c574654c36bc45aad3246ef5e
ChaoYue <hechaoyue0307@gmail.com>
2017-01-07 10:14:27 +0800

    debugged for the build

---
+++ dist/static/js/app.96b12318252ef303b50f.js
@@ -0,0 +1,19 @@
+webpackJsonp([1,0,2],[
+/* 0 */
+/***/ function(module, exports) {
+
+	// The Vue build version to load with the `import` command
+	// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+	import Vue from 'vue';
+	import App from './App';
+	
+	/* eslint-disable no-new */
+	new Vue({
+	  el: '#app',
+	  template: '<App/>',
+	  components: { App }
+	});
+
+/***/ }
+]);
+//# sourceMappingURL=app.96b12318252ef303b50f.js.map
