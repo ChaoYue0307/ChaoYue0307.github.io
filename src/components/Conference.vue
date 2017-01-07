e93276e92cb935267fd02d67de880efadc45f12e
ChaoYue <hechaoyue0307@gmail.com>
2017-01-07 11:55:40 +0800

    Added Material Vue for the project

--- src/components/Conference.vue
+++ src/components/Conference.vue
@@ -1,11 +1,16 @@
 <template>
   <div>
     <p>Conference Timetable</p>
-    <button class="btn btn-primary">Test</button>
+    <md-button class="md-raised">Default</md-button>
+    <md-button class="md-raised md-primary">Primary</md-button>
+    <md-button class="md-raised md-accent">Accent</md-button>
+    <md-button class="md-raised md-warn">Warn</md-button>
+    <md-button class="md-raised md-primary" disabled>Disabled</md-button>
+    <md-button class="md-raised md-dense">Dense</md-button>
   </div>
 </template>
 
 <script>
-    export default{
-    }
+  export default {
+  }
 </script>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,9 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueMaterial from 'vue-material'
+
+
+Vue.use(VueMaterial);
 
 new Vue({
   el: '#app',
