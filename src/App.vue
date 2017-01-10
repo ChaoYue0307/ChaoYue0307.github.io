c8b5f6a368361f7eddcf49dd5f37fadb40f72bb1
ChaoYue <hechaoyue0307@gmail.com>
2017-01-10 16:05:36 +0800

    Added an avatar to the web, still considering the http secure issue, other wise it may not be easily accessed, may migrate to other web domain?

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
+    <img src="./assets/logo.png" width="15%">
     <h1>{{ msg }}</h1>
     <app-conference></app-conference>
   </div>
