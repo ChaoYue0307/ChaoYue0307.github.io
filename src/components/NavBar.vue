b7134c99881de9e4d1b130fbd809002d9897db70
ChaoYue <hechaoyue0307@gmail.com>
2017-01-10 17:14:13 +0800

    Added a background color to the website

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -6,7 +6,6 @@
         <li><a href="https://www.facebook.com/profile.php?id=100003276260924">Facebook</a></li>
         <li><a href="https://www.youtube.com/channel/UCcFj2aH7okYpx_R9GY4uPQw">Youtube</a></li>
         <li><a href="http://weibo.com/2671410111/profile?rightmod=1&wvr=6&mod=personinfo&is_all=1">Weibo</a></li>
-
     </ul>
 
     </div>
