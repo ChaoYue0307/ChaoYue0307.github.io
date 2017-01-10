2fc72e856f69d6374b3d49efe5dc1dee25b7f0c7
ChaoYue <hechaoyue0307@gmail.com>
2017-01-10 17:19:56 +0800

    Added a text info to indicate if the iframe cannot be loaded

--- src/components/Conference.vue
+++ src/components/Conference.vue
@@ -4,7 +4,7 @@
       <h2>Conferences</h2>
       <div>
         <iframe src="http://www.confsearch.org/faces/pages/staticresults.jsp?query=podc%20stoc%20focs%20soda%20aaai%20ijcai%20aamas%20kdd%20icalp%20stacs%20ec%20www&sortMode=1&graphicView=1"
-          height="700px" width="900px" name="confsearch_frame" frameborder="0" id="ifr"> </iframe>
+          height="700px" width="900px" name="confsearch_frame" frameborder="0" id="ifr"> The iframe is not suitable displayed</iframe>
       </div>
 
     </div>
