edd9fa2a5d9b91f1fdc9cefaf1abc188218fd730
ChaoYue <hechaoyue0307@gmail.com>
2017-01-13 13:45:01 +0800

    Added icons for other items in footbarvue

--- src/components/FooterBar.vue
+++ src/components/FooterBar.vue
@@ -2,19 +2,19 @@
   <div>
     <ul>
       <li>
-        <md-button href="http://hechaoyue.com" class="md-primary">Blog</md-button>
+        <md-button href="http://hechaoyue.com" class="md-primary"><i class="fa fa-rss" aria-hidden="true"> Blog</i></md-button>
       </li>
       <li>
         <md-button href="https://github.com/ChaoYue0307" class="md-primary"><i class="fa fa-github" aria-hidden="true"> Github</i></md-button>
       </li>
       <li>
-        <md-button href="https://www.facebook.com/profile.php?id=100003276260924" class="md-primary">Facebook</md-button>
+        <md-button href="https://www.facebook.com/profile.php?id=100003276260924" class="md-primary"><i class="fa fa-facebook" aria-hidden="true"> Facebook</i></md-button>
       </li>
       <li>
-        <md-button href="https://www.youtube.com/channel/UCcFj2aH7okYpx_R9GY4uPQw" class="md-primary">Youtube</md-button>
+        <md-button href="https://www.youtube.com/channel/UCcFj2aH7okYpx_R9GY4uPQw" class="md-primary"><i class="fa fa-youtube" aria-hidden="true"> Youtube</i></md-button>
       </li>
       <li>
-        <md-button href="http://weibo.com/2671410111/profile?rightmod=1&wvr=6&mod=personinfo&is_all=1" class="md-primary">Weibo</md-button>
+        <md-button href="http://weibo.com/2671410111/profile?rightmod=1&wvr=6&mod=personinfo&is_all=1" class="md-primary"><i class="fa fa-weibo" aria-hidden="true"> Weibo</i></md-button>
       </li>
     </ul>
   </div>
