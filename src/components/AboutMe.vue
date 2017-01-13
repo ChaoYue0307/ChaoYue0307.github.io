dfbd618b728c030e01dddd63ae1c62a3a13188a4
ChaoYue <hechaoyue0307@gmail.com>
2017-01-13 11:03:15 +0800

    Roughly added some content to the aboutme vue page and will modify it to a better stage in the future

--- src/components/AboutMe.vue
+++ src/components/AboutMe.vue
@@ -1,6 +1,12 @@
 <template>
     <div>
         <h2>About Me</h2>
+        <p>Hi Buddies! I am a Phd student in the field of Computer Science at
+            <a href="https://youtu.be/7s8GH0IhHYA">Nanyang Technological University</a>, Singapore.</p>
+        <p>My research interests include Artificial Intelligence, Machine Learning, Deep Learning , Data Science and so on.</p>
+        <p> I also quite like building applications such as Web, Android, iOS, Game and so on in my work and leiture time.</p>
+        <p> You can know me and my projects or research better if you go to the social network links provided above.</p>
+        <p> You are also welcome to <a href="mailto:hechaoyue0307@gmail.com">drop me an email</a> if you have interest to discuss related stuff with me.</p>
     </div>
 </template>
 
