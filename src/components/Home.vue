0f4e6acbd24f4d43c7a2e78f5a7d54075b100a52
ChaoYue <hechaoyue0307@gmail.com>
2017-01-13 09:54:38 +0800

    Added a Home vue to separate the home page and register a route for it

---
+++ src/components/Home.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <app-navbar></app-navbar>
+    <app-conference></app-conference>    
+  </div>
+</template>
+
+<script>
+import Conference from './Conference.vue';
+import NavBar from './NavBar.vue';
+
+export default {
+  components: {
+    appConference: Conference,
+    appNavbar: NavBar
+  }
+}
+</script>
---
+++ src/routes.js
@@ -0,0 +1,7 @@
+import Home from './components/Home.vue';
+import AboutMe from './components/AboutMe.vue';
+
+export const routes = [
+    { path: '/', component: Home },
+    { path: '/AboutMe', component: AboutMe }  
+];
