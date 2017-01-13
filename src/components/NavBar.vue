53e978026eaa4ef2da1352ae9350a643f1b93e04
ChaoYue <hechaoyue0307@gmail.com>
2017-01-13 10:49:58 +0800

    Modified the nav bar interms of styling, use router-link to link the route to specific page

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,14 +1,31 @@
 <template>
   <div>
-    <ul display="inline">
-        <li><a href="http://hechaoyue.com">Blog<a></li>
-        <li><a href="https://github.com/ChaoYue0307">Github</a></li>
-        <li><a href="https://www.facebook.com/profile.php?id=100003276260924">Facebook</a></li>
-        <li><a href="https://www.youtube.com/channel/UCcFj2aH7okYpx_R9GY4uPQw">Youtube</a></li>
-        <li><a href="http://weibo.com/2671410111/profile?rightmod=1&wvr=6&mod=personinfo&is_all=1">Weibo</a></li>
+    <ul>
+      <li>
+        <md-button class="md-accent"><router-link to="/">Home</router-link></md-button>
+      </li>
+      <li>
+        <md-button class="md-accent"><router-link to="/AboutMe">About Me</router-link></md-button>
+      </li>
     </ul>
-
-    </div>
+    <ul>
+      <li>
+        <md-button href="http://hechaoyue.com" class="md-primary">Blog</md-button>
+      </li>
+      <li>
+        <md-button href="https://github.com/ChaoYue0307" class="md-primary">Github</md-button>
+      </li>
+      <li>
+        <md-button href="https://www.facebook.com/profile.php?id=100003276260924" class="md-primary">Facebook</md-button>
+      </li>
+      <li>
+        <md-button href="https://www.youtube.com/channel/UCcFj2aH7okYpx_R9GY4uPQw" class="md-primary">Youtube</md-button>
+      </li>
+      <li>
+        <md-button href="http://weibo.com/2671410111/profile?rightmod=1&wvr=6&mod=personinfo&is_all=1" class="md-primary">Weibo</md-button>
+      </li>
+    </ul>
+  </div>
 </template>
 
 <script>
